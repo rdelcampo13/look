c02b6cc3b0a376c7f629fbfcefc9d439580463a4
Rudy Del Campo <r.delcampo13@gmail.com>
2018-11-26 01:01:11 -0600

    Initial Build

---
+++ build/precache-manifest.db5ada60da48c11aed5d20feda85cb75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e3fff68e19742e52757",
+    "url": "/static/js/main.4e3fff68.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "4e3fff68e19742e52757",
+    "url": "/static/css/main.90684381.chunk.css"
+  },
+  {
+    "revision": "5e438d9c7fe4ee62c35255ead4b4d62b",
+    "url": "/index.html"
+  }
+];
